80aa83ffdfa1a03dd89d1fbfd46384a20f8c8b83
Dmitry <killer_code@bk.ru>
2021-03-08 23:41:27 +0400

    start front admin && create empty layout

---
+++ pages/admin/login.vue
@@ -0,0 +1,35 @@
+<template>
+  <section class="app-form-wrapper">
+    <v-card class="card mt-16" width="500px" outlined shaped tile>
+      <section class="card__header pa-6">
+        <v-card-title class="pa-0">
+          Авторизация
+        </v-card-title>
+      </section>
+      <section class="card__body pa-6">
+
+      </section>
+    </v-card>
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'Login',
+  layout: 'empty',
+}
+</script>
+
+<style lang="scss" scoped>
+.app-form-wrapper {
+  display: flex;
+  justify-content: center;
+  height: 100vh;
+}
+.card {
+  height: fit-content;
+  &__header {
+    background-color: #3e3e3e;
+  }
+}
+</style>
---
+++ pages/admin/test.vue
@@ -0,0 +1,12 @@
+<template>
+  <section>
+    <h1>TEST</h1>
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'Test',
+  layout: 'admin',
+}
+</script>
